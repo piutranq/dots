735d50665cf7c7aa9503406a12219a607b97588b
piutranq <piutranq@hotmail.com>
2019-09-25 17:53:35 +0900

    fix some rofi-scripts

--- stow/rofi/.config/rofi/scripts/rofi-dmenu-nerdfont.sh
+++ stow/rofi/.config/rofi/scripts/rofi-dmenu-nerdfont.sh
@@ -7,10 +7,10 @@ NF_LIST="$ROFI_DIR/nerdfont-cheatsheet"
 
 TITLE=""
 
-SELECTED=exec cat $NF_LIST |\
+SELECTED=$(cat $NF_LIST |\
     rofi    -dmenu -i\
     -p      $TITLE\
     -config $ROFI_CONF\
-    -theme  $ROFI_THEME
+    -theme  $ROFI_THEME)
 
 echo $SELECTED | awk '{ printf $2 }' | xclip -selection CLIPBOARD
