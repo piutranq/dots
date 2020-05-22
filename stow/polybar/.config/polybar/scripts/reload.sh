fbee7f46d2cbb846ed33cb4d3b8ec47778ea962f
piutranq <piutranq@hotmail.com>
2020-05-22 23:11:38 +0900

    Add gruvbox-material scheme at cologen-refresh +@
    - Add gruvbox-material color scheme at cologen-refresh
    - Add polybar device-specific config for "honami"
    - Add the check_device_specific() at polybar launch script:
        It check the device-specific config and make symlink for current hostname

--- stow/polybar/.config/polybar/scripts/reload.sh
+++ stow/polybar/.config/polybar/scripts/reload.sh
@@ -3,6 +3,12 @@ wait_for_launch () {
     while [ ! $(find /tmp/polybar_mqueue.*) ]; do sleep 0.1; done
 }
 
+check_device_specific () {
+    local -r DEV_LINKNAME="$XDG_CONFIG_HOME/polybar/device-specific/current"
+    local -r DEV_TARGET="$XDG_CONFIG_HOME/polybar/device-specific/$(hostname)"
+    [ -r $DEV_LINKNAME ] || ln -sf -T "$DEV_TARGET" -- "$DEV_LINKNAME"
+}
+
 launch-all () {
     polybar top &
 }
@@ -16,6 +22,7 @@ is_launched () {
 }
 
 main () {
+    check_device_specific
     is_launched && reload-all || launch-all
     wait_for_launch
 }
